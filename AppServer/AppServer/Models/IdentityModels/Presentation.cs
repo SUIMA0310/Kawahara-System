d7256fbba13935fb0a49256aeec8557be2eedec7
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-24 16:53:55 +0000

    Add : Presentation
    プレゼンテーションのModelクラスとその関連クラスを追加

---
+++ AppServer/AppServer/Models/IdentityModels/Presentation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppServer.Models
+{
+    /// <summary>
+    /// プレゼンテーション
+    /// </summary>
+    public class Presentation
+    {
+
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Presentationの管理名（重複を許容）
+        /// </summary>
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 利用可能なリアクション
+        /// </summary>
+        [Required]
+        public eReactionType HasReaction { get; set; } = eReactionType.Good;
+
+        /// <summary>
+        /// 合計リアクション回数
+        /// </summary>
+        public ulong ReactionCount { get; set; } = 0;
+
+        /// <summary>
+        /// 所有者
+        /// </summary>
+        [Required]
+        public virtual ApplicationUser Owner { get; set; }
+
+    }
+}
---
+++ AppServer/AppServer/Models/IdentityModels/eReactionType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AppServer.Models
+{
+    /// <summary>
+    /// リアクションの種類
+    /// </summary>
+    [Flags]
+    public enum eReactionType
+    {
+
+        /// <summary>
+        /// いいね！
+        /// </summary>
+        Good,
+
+        /// <summary>
+        /// ナイス！
+        /// </summary>
+        Nice,
+
+        /// <summary>
+        /// おもしろいね！
+        /// </summary>
+        Fun
+
+    }
+}
