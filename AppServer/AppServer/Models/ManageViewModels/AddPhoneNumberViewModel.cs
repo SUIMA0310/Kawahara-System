845fb16e7773b9809c9196d73dad5de83326b8d9
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-24 15:17:38 +0000

    Clean : Models
    Models の中の、ファイルと型を整理

---
+++ AppServer/AppServer/Models/ManageViewModels/AddPhoneNumberViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppServer.Models
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "電話番号")]
+        public string Number { get; set; }
+    }
+}
---
+++ AppServer/AppServer/Models/ManageViewModels/VerifyPhoneNumberViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppServer.Models
+{
+    public class VerifyPhoneNumberViewModel
+    {
+        [Required]
+        [Display(Name = "コード")]
+        public string Code { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "電話番号")]
+        public string PhoneNumber { get; set; }
+    }
+}
