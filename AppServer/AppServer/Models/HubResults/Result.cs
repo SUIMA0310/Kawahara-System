c7f4a8e6422f3702224e1cd46fa0f41dccf2a2db
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-25 11:47:05 +0000

    Update : ReactionHub
    ReactionHubを実装

---
+++ AppServer/AppServer/Models/HubResults/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppServer.Models.HubResults
+{
+    public class Result
+    {
+
+        public Result(eResultTypes resultTypes, string message = null)
+        {
+            this.ResultTypes = resultTypes;
+            this.Message = message ?? string.Empty;
+        }
+
+        public eResultTypes ResultTypes { get; }
+        public string Message { get; }
+
+    }
+}
