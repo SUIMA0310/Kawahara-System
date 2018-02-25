16d960645807ba7effaf0995625ed5ba7e8eefd8
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-25 22:42:03 +0000

    Clean : コメントの追加

--- DesktopApp/Bootstrapper.cs
+++ DesktopApp/Bootstrapper.cs
@@ -6,22 +6,38 @@ using Prism.DryIoc;
 
 namespace DesktopApp
 {
-    class Bootstrapper : DryIocBootstrapper
+
+    /// <summary>
+    /// アプリケーションの起動処理
+    /// </summary>
+    public class Bootstrapper : DryIocBootstrapper
     {
+
+        /// <summary>
+        /// MainWindowの生成
+        /// </summary>
+        /// <returns>Windowのインスタンス</returns>
         protected override DependencyObject CreateShell()
         {
-            return Container.Resolve<MainWindow>();
+            return this.Container.Resolve<MainWindow>();
         }
 
+        /// <summary>
+        /// MainWindowの初期化（表示）
+        /// </summary>
         protected override void InitializeShell()
         {
             Application.Current.MainWindow.Show();
         }
 
+        /// <summary>
+        /// Moduleの登録
+        /// </summary>
         protected override void ConfigureModuleCatalog()
         {
-            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            var moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
             //moduleCatalog.AddModule(typeof(YOUR_MODULE));
         }
+
     }
 }
