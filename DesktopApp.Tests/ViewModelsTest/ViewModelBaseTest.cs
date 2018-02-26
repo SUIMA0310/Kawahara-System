9924881c682764e48d8433af4e33cc45d9880b69
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 12:31:55 +0000

    Add : ViewModelBase
    ViewModel の スーパークラスを追加

---
+++ DesktopApp.Tests/ViewModelsTest/ViewModelBaseTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesktopApp.Tests.ViewModelsTest
+{
+    [TestClass]
+    public class ViewModelBaseTest
+    {
+        [TestMethod]
+        public void Title()
+        {
+
+            var viewModel = new Core.ViewModels.ViewModelBase();
+
+            Assert.IsNotNull(viewModel.Title.Value);
+
+        }
+
+        [TestMethod]
+        public void Dispose()
+        {
+
+            var viewModel = new Core.ViewModels.ViewModelBase();
+
+            viewModel.Dispose();
+
+        }
+    }
+}
