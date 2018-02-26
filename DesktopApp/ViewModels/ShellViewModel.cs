0c8504a9c490b51af3c067fb340f4b0040ce5182
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 12:45:42 +0000

    Change : ShellViewModelのスーパークラスを変更
    Prismのスーパークラスから、DesktopApp.Coreのスーパークラスに変更

--- DesktopApp/ViewModels/ShellViewModel.cs
+++ DesktopApp/ViewModels/ShellViewModel.cs
@@ -2,17 +2,13 @@
 
 namespace DesktopApp.ViewModels
 {
-    public class ShellViewModel : BindableBase
+    public class ShellViewModel : Core.ViewModels.ViewModelBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
+        
         public ShellViewModel()
         {
+
+            this.Title.Value = "Prism Application";
 
         }
     }
