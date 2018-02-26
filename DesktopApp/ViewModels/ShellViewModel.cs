9df3282a879d9cfb6e22af396e2e8dbea2958427
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 11:04:57 +0000

    fix : ViewModel の名前を変更
    Shell の ViewModel を MainWindowViewModel から ShellViewModel に変更

--- DesktopApp/ViewModels/ShellViewModel.cs
+++ DesktopApp/ViewModels/ShellViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace DesktopApp.ViewModels
 {
-    public class MainWindowViewModel : BindableBase
+    public class ShellViewModel : BindableBase
     {
         private string _title = "Prism Application";
         public string Title
@@ -11,7 +11,7 @@ namespace DesktopApp.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        public ShellViewModel()
         {
 
         }
