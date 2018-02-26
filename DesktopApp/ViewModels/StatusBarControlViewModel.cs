440227262f5070760ced629fb9b84f98139e3961
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 20:26:12 +0000

    Update : StatusBar と StatusService を連携

--- DesktopApp/ViewModels/StatusBarControlViewModel.cs
+++ DesktopApp/ViewModels/StatusBarControlViewModel.cs
@@ -3,14 +3,24 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using DesktopApp.Services;
 
 namespace DesktopApp.ViewModels
 {
-    public class StatusBarControlViewModel : BindableBase
+    public class StatusBarControlViewModel : ViewModelBase
     {
-        public StatusBarControlViewModel()
-        {
 
+        public ReactiveProperty<string> StatusMessage { get; }
+
+        public StatusBarControlViewModel( IStatusService statusService )
+        {
+            this.Title.Value = "StatusBar";
+            this.StatusMessage = statusService
+                .Status
+                .ToReactiveProperty()
+                .AddTo( this.Disposable );
         }
     }
 }
