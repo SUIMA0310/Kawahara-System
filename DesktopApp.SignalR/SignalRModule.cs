17a1eecf3f7aa0abb9300d53bce6ac56432fa935
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 13:52:10 +0000

    Add : DesktopApp.SignalR
    プロジェクトの追加と、参照の設定

---
+++ DesktopApp.SignalR/SignalRModule.cs
@@ -0,0 +1,26 @@
+﻿using DesktopApp.SignalR.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using DryIoc;
+using Prism.DryIoc;
+
+namespace DesktopApp.SignalR
+{
+    public class SignalRModule : IModule
+    {
+        private IRegionManager _regionManager;
+        private IContainer _container;
+
+        public SignalRModule(IContainer container, IRegionManager regionManager)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _container.RegisterTypeForNavigation<ViewA>();
+        }
+    }
+}
