45766f3445e890a01e50e9393c21adf807c0e7a3
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 19:49:08 +0000

    Add : StatusService

---
+++ DesktopApp.Core/Services/eStatusMessages.cs
@@ -0,0 +1,28 @@
+﻿namespace DesktopApp.Services
+{
+    public enum eStatusMessages
+    {
+
+        Ready,
+        processing,
+
+
+    }
+
+    public static class StatusMessagesExpansion
+    {
+
+        private static readonly string[] _names =
+        {
+            "準備完了",
+            "しばらくお待ちください"
+        };
+
+        public static string DisplayName(this eStatusMessages message)
+        {
+            return _names[(int)message];
+        }
+
+    }
+
+}
