ad103b0b4ca0ce5fb0920e6308c6a457b323b467
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-26 23:02:36 +0000

    Add : OverlayWindow

---
+++ DesktopApp.Overlay.Test/OverlayWindowTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesktopApp.Overlay.Core;
+
+namespace DesktopApp.Overlay.Test
+{
+    [TestClass]
+    public class OverlayWindowTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+
+            new OverlayWindow();
+
+        }
+
+        [TestMethod]
+        public void AltF4Cancel()
+        {
+
+            var window = new OverlayWindow();
+
+            window.AltF4Cancel = true;
+            window.AltF4Cancel = false;
+            window.AltF4Cancel = true;
+            window.AltF4Cancel = false;
+
+            window.Show();
+            System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+
+            window.AltF4Cancel = true;
+            window.AltF4Cancel = false;
+            window.AltF4Cancel = true;
+            window.AltF4Cancel = false;
+
+            window.Close();
+
+        }
+
+        [TestMethod]
+        public void ClickThrough()
+        {
+
+            var window = new OverlayWindow();
+
+            window.ClickThrough = true;
+            window.ClickThrough = false;
+            window.ClickThrough = true;
+            window.ClickThrough = false;
+
+            window.Show();
+            System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+
+            window.ClickThrough = true;
+            window.ClickThrough = false;
+            window.ClickThrough = true;
+            window.ClickThrough = false;
+
+            window.Close();
+
+        }
+
+        [TestMethod]
+        public void Show()
+        {
+
+            var window = new OverlayWindow();
+
+            window.Show();
+
+            System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+
+            window.Close();
+
+        }
+
+    }
+}
