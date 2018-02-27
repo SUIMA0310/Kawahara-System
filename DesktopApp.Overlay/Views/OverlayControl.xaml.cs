a99ef66922573ba6c0d413b775480ebf67b9f575
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-27 14:58:58 +0000

    Add : Blanc OverlayControl View ＆ ViewModel

---
+++ DesktopApp.Overlay/Views/OverlayControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopApp.Views
+{
+    /// <summary>
+    /// Interaction logic for OverlayControl
+    /// </summary>
+    public partial class OverlayControl : UserControl
+    {
+        public OverlayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
