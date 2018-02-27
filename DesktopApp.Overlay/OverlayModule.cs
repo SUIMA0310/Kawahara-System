c821a0a33d81daee1835e54413a4f028a8e59e75
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-27 12:29:08 +0000

    Add : DesktopApp.Overlay Project

---
+++ DesktopApp.Overlay/OverlayModule.cs
@@ -0,0 +1,29 @@
+﻿using DryIoc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp
+{
+    public class OverlayModule : IModule
+    {
+
+        private IContainer Container { get; }
+        private IRegionManager RegionManager { get; }
+
+        public OverlayModule(IContainer container, IRegionManager regionManager)
+        {
+            this.Container = container;
+            this.RegionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+        }
+
+    }
+}
