0d3471d1c97bb858b87ccb0284373d15c44b786f
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-02-27 10:11:19 +0000

    Add : DesktopApp.Overlay.Draw Project.

---
+++ DesktopApp.Overlay.Draw/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp.Overlay.Draw
+{
+    public class Class1
+    {
+    }
+}
