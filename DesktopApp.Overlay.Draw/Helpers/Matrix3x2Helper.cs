776208f05ddf291d55baff41ee9b025acf47a27e
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-03-03 15:50:44 +0000

    Add : ObjectBase and GoodObject
    PathGeometry を描くためのクラス群

---
+++ DesktopApp.Overlay.Draw/Helpers/Matrix3x2Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Mathematics.Interop;
+
+namespace DesktopApp.Overlay.Draw.Helpers
+{
+    public static class Matrix3x2Helper
+    {
+
+        public static RawMatrix3x2 Identity = new RawMatrix3x2(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+
+        public static RawMatrix3x2 Translation(RawPoint point)
+        {
+            return new RawMatrix3x2(1.0f, 0.0f, 0.0f, 1.0f, point.X, point.Y);
+        }
+
+        public static RawMatrix3x2 Translation(float x, float y)
+        {
+            return new RawMatrix3x2(1.0f, 0.0f, 0.0f, 1.0f, x, y);
+        }
+
+        public static RawMatrix3x2 Translation(this RawMatrix3x2 baseMatrix, RawPoint point)
+        {
+            baseMatrix.M31 = point.X;
+            baseMatrix.M32 = point.Y;
+            return baseMatrix;
+        }
+
+        public static RawMatrix3x2 Translation(this RawMatrix3x2 baseMatrix, float x, float y)
+        {
+            baseMatrix.M31 = x;
+            baseMatrix.M32 = y;
+            return baseMatrix;
+        }
+
+    }
+}
---
+++ DesktopApp.Overlay.Draw/Views/ReactionControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D2dControl;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.DXGI;
+using SharpDX.Mathematics.Interop;
+using DesktopApp.Overlay.Draw.Helpers;
+
+namespace DesktopApp.Overlay.Draw.Views
+{
+    public class ReactionControl : D2dControl.D2dControl
+    {
+
+        public ReactionControl()
+        {
+
+            this.resCache.Add("Good", target => new Objects.GoodObject(target));
+
+        }
+
+        public override void Render(RenderTarget target)
+        {
+            target.Clear(null);
+
+            target.Transform = Matrix3x2Helper.Identity;
+            target.AntialiasMode = AntialiasMode.PerPrimitive;
+
+
+            var good = this.resCache["Good"] as Objects.ObjectBase;
+
+            good.Render(new RawPoint(150, 650), new RawColor4(1.0f, 0.25f, 0.25f, 0.5f));
+
+        }
+
+    }
+}
