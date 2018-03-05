fc064399fb733aa5d89201d364020da761dee341
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-03-05 22:40:03 +0000

    Add : PopupMetroWindowAction

---
+++ DesktopApp.Core/Actions/PopupMetroWindowAction.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesktopApp.Actions
+{
+    public class PopupMetroWindowAction : PopupWindowAction
+    {
+        protected override Window CreateWindow()
+        {
+            return new MetroRadiance.UI.Controls.MetroWindow();
+        }
+    }
+}
