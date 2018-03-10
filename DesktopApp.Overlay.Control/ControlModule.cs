1507f70f51727fb0d11c2d0116c9409a9223f85d
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-03-10 04:11:06 +0000

    Add : DesktopApp.Overlay.Control project.

---
+++ DesktopApp.Overlay.Control/ControlModule.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using System;
+using DryIoc;
+using Prism.DryIoc;
+
+namespace DesktopApp
+{
+    public class ControlModule : IModule
+    {
+        private IRegionManager RegionManager;
+        private IContainer Container;
+
+        public ControlModule(IContainer container, IRegionManager regionManager)
+        {
+            this.Container = container;
+            this.RegionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
