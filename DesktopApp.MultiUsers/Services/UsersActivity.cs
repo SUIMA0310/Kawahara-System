3c84120939099bbf648d7c65d2d80d3bfe7d88b2
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-03-16 06:27:36 +0000

    Update : UsersActivity

--- DesktopApp.MultiUsers/Services/UsersActivity.cs
+++ DesktopApp.MultiUsers/Services/UsersActivity.cs
@@ -18,6 +18,29 @@ namespace DesktopApp.Services
         public UsersActivity( IReactionHubProxy reactionHubProxy )
         {
             this.ReactionHubProxy = reactionHubProxy;
+            this.ReactionHubProxy
+                .OnReceiveReaction()
+                .Subscribe( x =>
+                {
+                    var user = this.SelectedUser.Value;
+                    if ( user == null ) {
+                        return;
+                    }
+                    lock ( user ) {
+                        switch ( x.Item1 ) {
+                            case Models.eReactionType.Good:
+                                user.GoodCount++;
+                                break;
+                            case Models.eReactionType.Nice:
+                                user.NiceCount++;
+                                break;
+                            case Models.eReactionType.Fun:
+                                user.FunCount++;
+                                break;
+                        }
+                    }
+                } )
+                .AddTo( this.Disposable );
 
             this.SelectedUser = new ReactivePropertySlim<IUser>();
             this.Users = new ReactiveCollection<IUser>();
@@ -26,8 +49,41 @@ namespace DesktopApp.Services
         public ReactivePropertySlim<IUser> SelectedUser { get; }
         public ObservableCollection<IUser> Users { get; }
 
-        public Type UserType { get; set; }
-        public Func<IUser> UserFactory { get; set; }
+        public void CountReset()
+        {
+            var user = this.SelectedUser.Value;
+            lock ( user ) {
+                user.GoodCount = 0;
+                user.NiceCount = 0;
+                user.FunCount  = 0;
+            }
+        }
+
+        public void AddUser( string userName )
+        {
+            var user = this.CreateUser();
+
+            if ( user == null ) {
+                throw new Exception( $"{nameof( CreateUser )} can not return null." );
+            }
+
+            user.Name = userName;
+            this.Users.Add( user );
+        }
+
+        public void RemoveUser( IUser user )
+        {
+            if ( this.SelectedUser.Value == user ) {
+                this.SelectedUser.Value = null;
+            }
+
+            this.Users.Remove( user );
+        }
+
+        protected virtual IUser CreateUser()
+        {
+            return new User();
+        }
 
     }
 }
