5af693dc8c688f068ec68cf56468755fb3e5e438
ASUS-MAINPC\SUIMA <mizumacrocus@yahoo.co.jp>
2018-03-16 04:28:18 +0000

    Add : User & UsersActivity

---
+++ DesktopApp.MultiUsers/Models/IUser.cs
@@ -0,0 +1,10 @@
+﻿namespace DesktopApp.MultiUsers.Models
+{
+    public interface IUser
+    {
+        string Name    { get; set; }
+        uint GoodCount { get; set; }
+        uint NiceCount { get; set; }
+        uint FunCount  { get; set; }
+    }
+}
---
+++ DesktopApp.MultiUsers/Models/UsersActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using DesktopApp.Services;
+using System.Reactive.Linq;
+using System.Collections.ObjectModel;
+
+namespace DesktopApp.MultiUsers.Models
+{
+    public class UsersActivity
+    {
+        private readonly IReactionHubProxy ReactionHubProxy;
+
+        public UsersActivity( IReactionHubProxy reactionHubProxy )
+        {
+            this.ReactionHubProxy = reactionHubProxy;
+
+            this.SelectedUser = new ReactivePropertySlim<IUser>();
+            this.Users = new ReactiveCollection<IUser>();
+        }
+
+        public ReactivePropertySlim<IUser> SelectedUser { get; }
+        public ObservableCollection<IUser> Users { get; }
+
+        public Type UserType { get; set; }
+        public Func<IUser> UserFactory { get; set; }
+
+    }
+}
